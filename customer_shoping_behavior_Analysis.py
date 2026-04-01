57e10106e57df728d3edcee7d14cbcf39c9ea2b5
Shubhi Nigam <khushinigam453@gmail.com>
2026-04-01 20:48:25 +0530

    Add data transformation and cleaning logic using pandas

---
+++ customer_shoping_behavior_Analysis.py
@@ -0,0 +1,42 @@
+import pandas as pd
+df=pd.read_csv('customer_shopping_behavior.csv')
+df.head()
+df.info()
+df.describe(include= 'all')
+df.isnull().sum()
+df['Review Rating']=df.groupby('Category')['Review Rating'].transform(lambda x:x.fillna(x.median()))
+df.isnull().sum()
+df.columns=df.columns.str.lower()
+df.columns=df.columns.str.replace(' ','_')
+df=df.rename(columns={'purchase_amount_(usd)':'purchase_amount'})
+# create a column age_group
+labels=['Young Adult','Adult','Middle-aged','senior']
+df['age_group']=pd.qcut(df['age'],q=4,labels=labels)
+# create column purchase_frequency_days
+frequency_mapping={
+    'Fortnightly':14,
+    'Weekly':7,
+    'Monthly':30,
+    'Quarterly':90,
+    'Bi-Weekly':14,
+    'Annually':365,
+    'Every 3 Months':90
+}
+df['purchase_frequency_days']=df['frequency_of_purchases'].map(frequency_mapping)
+df=df.drop('promo_code_used',axis=1)
+
+
+# database se connect
+import sqlite3
+
+# database create/connect (same folder me banega)
+conn = sqlite3.connect('customer_behavior.db')
+
+# dataframe ko table me convert karo
+df.to_sql('customer_data', conn, if_exists='replace', index=False)
+
+# save & close
+conn.commit()
+conn.close()
+
+print("✅ Data successfully stored in SQLite!")
